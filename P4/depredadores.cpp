ce31e8e5c1c5a76d9bd7b2235ae7948ef0a13fab
Jose26398 <jsg26398@gmail.com>
2020-01-05 17:53:14 +0100

    Codigos de la practica 4 terminados

---
+++ P4/depredadores.cpp
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <list>
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+float a11;
+float a12;
+float a21;
+float a22;
+
+float x;
+float y;
+float dt;
+
+const int numeq = 2;
+float t;
+float tinic;
+float tfin;
+
+float f[2];
+float oldestado[2];
+float estado[2];
+
+float incr;
+
+
+void salida(float t){
+	cout << std::fixed << std::setprecision(2) << "Depredadores: " << estado[1] << "\t\t"
+											   << "Presas: " 	   << estado[0] << "\t\t"
+											   << "Tiempo: " 	   << t << "\n";
+}
+
+
+void fijar_parametros(){
+	
+}
+
+
+void derivacion(float est[numeq], float f[numeq], float tt){
+	// específico para el modelo considerado
+	f[0] = a11*est[0] - a12*est[0]*est[1];
+	f[1] = a21*est[0]*est[1] - a22*est[1];
+}
+
+
+void one_step_runge_kutta(float inp[numeq], float out[numeq], float tt, float hh){
+	float k[numeq][4];
+
+	for (int i=0; i < numeq; i++)
+		out[i] = inp[i]; 
+	float time = tt;
+	
+	for (int j=0; j < 4; j++) {
+		derivacion(out, f, time);
+		for (int i=0; i < numeq; i++)
+			k[i][j] = f[i];
+		
+		if (j < 2) {incr = hh/2;}
+		else {incr = hh;}
+		
+		time = tt + incr;
+		for (int i=0; i < numeq; i++)
+			out[i] = inp[i] + k[i][j]*incr;
+	}
+
+	for (int i=0; i < numeq; i++)
+		out[i] = inp[i] + hh/6  *  (k[i][0] + 2*k[i][1] + 2*k[i][2] + k[i][3]);
+}
+
+
+void one_step_euler(float inp[numeq], float out[numeq], float tt, float hh){
+	derivacion(inp, f, tt);
+	for (int i=0; i < numeq; i++)
+		out[i] = inp[i] + hh*f[i];
+}
+
+
+void integracion(){
+	do{
+		salida(t);
+		oldestado[0] = estado[0];
+		oldestado[1] = estado[1];
+		one_step_runge_kutta(oldestado, estado, t, dt); //sustituir por one-step-runge-kutta o por one-step-euler
+		t += dt;
+	} while(t < tfin);
+}
+
+
+int main(int argc, char *argv[]){
+	if(argc == 5){
+		sscanf(argv[1], "%f", &a11);
+		sscanf(argv[2], "%f", &a12);
+		sscanf(argv[3], "%f", &a21);
+		sscanf(argv[4], "%f", &a22);
+	}else{
+		printf("\nArgumentos: <a11> <a12> <a21> <a22>");
+		exit(1);
+	}
+
+	estado[0] = a22/a21;
+	estado[1] = a11/a12;
+	dt = 0.1;
+
+	tinic = 0;
+	tfin = 100;
+
+
+	// fijar_parametros();
+	t = tinic;
+	integracion();
+}
---
+++ P4/depredadores.py
@@ -0,0 +1,46 @@
+import os
+import re
+import sys
+from matplotlib import pyplot as plt
+
+# Ruta de tu ejecutable con respecto al script.
+EXECUTABLE_PATH = './depredadores'
+
+# Expresiones regulares de los valores a buscar.
+depredadores_pattern = re.compile(r'Depredadores: ([0-9]+\.[0-9]+)')
+presas_pattern = re.compile(r'Presas: ([0-9]+\.[0-9]+)')
+tiempo_pattern = re.compile(r'Tiempo: ([0-9]+\.[0-9]+)')
+
+# Input parameters
+try:
+    a11 = float(sys.argv[1])
+    a12 = float(sys.argv[2])
+    a21 = float(sys.argv[3])
+    a22 = float(sys.argv[4])
+except IndexError:
+    print("Numero de argumentos invalido")
+    exit()
+
+# Ejecuta el comando y guarda el resultado en una variable.
+cmd = f"{EXECUTABLE_PATH} {a11} {a12} {a21} {a22}"
+result = os.popen(cmd).read()
+
+# Busca todos los valores y los mapea a floats o enteros ya que son strings
+# y para dibujarlos tienen que ser numeros.
+depredadores = list(map(float, re.findall(depredadores_pattern, result)))
+presas = list(map(float, re.findall(presas_pattern, result)))
+tiempo = list(map(float, re.findall(tiempo_pattern, result)))
+
+# Dibuja los datos
+plt.plot(tiempo, depredadores, label="Depredadores")
+plt.plot(tiempo, presas, label="Presas")
+
+plt.xlabel('Tiempo')
+plt.ylabel('Poblacion')
+
+plt.tight_layout()
+
+# Guarda la gráfica con el nombre del ejecutable y los parametros dados.
+plt.legend(title='Poblacion', loc='upper right')
+plt.savefig(f"./doc/img/grafica.png")
+plt.show()
